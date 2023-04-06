9f5a1d6a6aaff07cec32edea210ce24009246c10
Dustin Wojciechowski <dustin.wojciechowski@microsoft.com>
2023-04-05 17:51:34 -0700

    Initial Commit

---
+++ TestingAppDockNameXCode/TestingAppDockNameXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAppDockNameXCodeApp.swift
+//  TestingAppDockNameXCode
+//
+//  Created by Dustin Wojciechowski on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingAppDockNameXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
